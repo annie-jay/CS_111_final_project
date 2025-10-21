317c97e6945f2d4d3ae241dd31cb07e96c08002e
annie-jay <jaya@carleton.edu>
2025-10-21 18:16:07 -0500

    Create final_project_Priest_Jay_CS11FA25.py

---
+++ final_project_Priest_Jay_CS11FA25.py
@@ -0,0 +1,65 @@
+# Intro CS Final Project
+
+def getRandomWord():
+    """
+        input: word list
+        output: correctWord
+        side effect: graphs window with word message
+    """
+    pass
+
+def drawMainBoard():
+    """
+        Generate user interface
+    """
+    pass
+
+def drawShape():
+    """
+        input: none
+        output: none
+        side effect: draws shape based on user clicks and inputs
+    """
+    pass
+
+def drawCircle():
+    """
+        input: none
+        output: none
+        side effect: draws circle based on user clicks and inputs
+    """
+def drawRect():
+
+def drawLine():
+
+def drawOval():
+
+
+
+def getAndCheckGuess():
+    """
+        input: guess
+        output: none
+        side effect: updates guessVaule: ends game if guessValue == true,
+            takes away life if guessValue == false
+    """
+    pass
+
+def gameOver():
+    """
+        input: none
+        output: none
+        side effect: if lives == 0, or guessValue == true, ends game
+    """
+    pass
+
+
+
+def main():
+    wordlist = "dog"
+    currentWord = getRandomWord(wordlist)
+    drawMainBoard()
+
+
+
+main()
